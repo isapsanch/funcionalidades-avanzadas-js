0df94d8681d3ee7e21ab30f1b3a17b343350d111
isapsanch <isabelpsanch@hotmail.com>
2023-11-14 02:47:36 +0100

    Añadir Ejercicio 09

--- src/ejercicio-09/index.js
+++ src/ejercicio-09/index.js
@@ -1,1 +1,12 @@
 // Escribe tu código aquí:
+const colors = new Set()
+
+colors.add('Rojo')
+colors.add('Verde')
+colors.add('Azul')
+colors.add('Rojo') // Intentar agregar un color duplicado
+
+console.log('Colores en el Set:', colors)
+
+const colorToCheck = 'Verde'
+console.log(`¿${colorToCheck} existe en el Set? ${colors.has(colorToCheck)}`)
