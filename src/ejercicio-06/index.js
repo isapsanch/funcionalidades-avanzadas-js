d1d811e40914b6afbdf64402e6232682cd8e0ef4
isapsanch <isabelpsanch@hotmail.com>
2023-11-14 02:38:31 +0100

    Añadir ejercicio 06

--- src/ejercicio-06/index.js
+++ src/ejercicio-06/index.js
@@ -1,1 +1,7 @@
 // Escribe tu código aquí:
+const fruits = ['Manzana', 'Plátano', 'Naranja', 'Uva']
+
+const [firstFruit, , , lastFruit] = fruits
+
+console.log(`Primera fruta: ${firstFruit}`)
+console.log(`Última fruta: ${lastFruit}`)
