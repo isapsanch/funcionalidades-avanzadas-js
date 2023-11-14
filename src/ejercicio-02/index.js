50329d35aa65759b137da928a0c64c30f8e5553b
isapsanch <isabelpsanch@hotmail.com>
2023-11-14 02:18:52 +0100

    Añadir ejericicio 02

--- src/ejercicio-02/index.js
+++ src/ejercicio-02/index.js
@@ -1,1 +1,17 @@
 // Escribe tu código aquí:
+
+const person = {
+    name: 'John',
+    greet: function () {
+        console.log(`Hola, soy ${this.name}`)
+    }
+}
+person.greet()
+
+const person2 = {
+    name:'Alice',
+    greet: () => { 
+        console.log(`Hola, soy ${this.name}`)
+    }
+}
+person2.greet()
